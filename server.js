be8433a6752d4a09c117fcc7465b88072ca2277b
Mykola Poberezhnyi <n.poberezhnyi@gmail.com>
2020-02-07 15:42:14 +0200

    add https

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+const fs             = require('fs');
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
+const https          = require('https');
 const db             = require('./config/db');
 const cors           = require('cors')
 const app            = express();
 const port = 8000;
+
+const privateKey  = fs.readFileSync('ca.key', 'utf8');
+const certificate = fs.readFileSync('ca.crt', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
+
+
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,7 +23,8 @@ MongoClient.connect(db.url, { useNewUrlParser: true }, (err, database) => {
     ${err}
     -----------------------------`);
     require('./app/routes')(app, database.db());
-    app.listen(port, () => {
+    var httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(port, () => {
         console.log('We are live on ' + port);
     });               
 })
