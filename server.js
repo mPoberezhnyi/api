a2acd80212cd868e6ade6cd8f7ada6fff1ce3e5a
Mykola Poberezhnyi <n.poberezhnyi@gmail.com>
2020-02-09 19:22:34 +0200

    add http

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const https          = require('https');
+const http           = require('http');
 const db             = require('./config/db');
 const cors           = require('cors')
 const app            = express();
 const port = 8000;
+const portHttp = 8030;
 
 const privateKey  = fs.readFileSync('ca.key', 'utf8');
 const certificate = fs.readFileSync('ca.crt', 'utf8');
@@ -24,7 +26,11 @@ MongoClient.connect(db.url, { useNewUrlParser: true }, (err, database) => {
     -----------------------------`);
     require('./app/routes')(app, database.db());
     var httpsServer = https.createServer(credentials, app);
+    var httpServer = http.createServer(app);
     httpsServer.listen(port, () => {
         console.log('We are live on ' + port);
-    });               
+    });
+    httpServer.listen(portHttp, () => {
+        console.log('We are live on ' + portHttp);
+    });
 })
